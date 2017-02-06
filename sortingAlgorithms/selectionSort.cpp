e18b07ad43a3c41fceab2f953bbb808fea5705b0
CBR0MS <cb@christianbroms.com>
2017-02-05 20:22:16 -0800

    Create selectionSort.cpp

---
+++ sortingAlgorithms/selectionSort.cpp
@@ -0,0 +1,22 @@
+void selectionSort(int arr[], int SIZE_ARR) {
+
+	for (int i = 0; i < SIZE_ARR - 1; i++) {
+
+		int indexMin = i + 1;
+		int assmMin = arr[i];
+		int subMin = arr[i + 1];
+
+		for (int j = i + 2; j < SIZE_ARR; j++) {
+
+			if (arr[j] < subMin) {
+
+				subMin = arr[j];
+				indexMin = j;
+			}
+		}
+		if (assmMin > subMin) {
+
+			swap(arr[indexMin], arr[i]);
+		}
+	}
+}
