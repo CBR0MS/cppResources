0d89b2fd33aa93037da2b50b45f321a56ecb65fc
Christian Broms <cb@christianbroms.com>
2017-02-16 20:00:10 -0800

    Create List.cpp

---
+++ List/List.cpp
@@ -0,0 +1,72 @@
+#include "List.h"
+#include <iostream>
+
+using namespace std;
+
+List::List()
+{
+	dataList = new int[10];
+	length = 0;
+	capacity = 10;
+}
+
+List::List(int p_cap)
+{
+	dataList = new int[p_cap];
+	length = 0;
+	capacity = p_cap;
+}
+
+List::List(const List& p_list)
+{
+	//Allocate separate memory
+	dataList = new int[p_list.capacity];
+
+	//Copy over attriutes of p_obj 
+	capacity = p_list.capacity;
+	length = p_list.length;
+
+	for (int i = 0; i < p_list.capacity; i++) {
+		dataList[i] = p_list.dataList[i];
+	}
+}
+
+void List::addItem(int newItem)
+{
+	if (length != capacity)
+	{
+		dataList[length] = newItem;
+		length++;
+	}
+}
+void List::printList()
+{
+	for (int i = 0; i < length; i++)
+		cout << dataList[i] << ' ';
+}
+void List::decrementAll()
+{
+	for (int i = 0; i < length; i++)
+		dataList[i]--;
+}
+
+List& List::operator= (const List& p_list)
+{
+	//Allocate separate memory
+	dataList = new int[p_list.capacity];
+
+	//Copy over attriutes of p_obj 
+
+	capacity = p_list.capacity;
+	length = p_list.length;
+
+	for (int i = 0; i < p_list.capacity; i++) {
+		dataList[i] = p_list.dataList[i];
+	}
+
+	return *this;
+}
+
+//List::~List()
+//{
+//}
