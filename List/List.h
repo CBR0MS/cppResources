769912f8fedf2708c36b15d0a7f1da213f745f59
Christian Broms <cb@christianbroms.com>
2017-02-16 19:59:26 -0800

    Create List.h

---
+++ List/List.h
@@ -0,0 +1,24 @@
+#ifndef ListClass
+#define ListClass
+
+class List
+{
+public:
+	List();
+	List(int);
+	List(const List&);
+
+	void addItem(int newItem);
+	void printList();
+	void decrementAll();
+
+	List& operator= (const List&);
+	//~List();
+
+private:
+	int *dataList;
+	int length;
+	int capacity;
+};
+
+#endif /* ListClass */
