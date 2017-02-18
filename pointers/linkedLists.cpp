b2f1011c4d6d1f1be72b5fcd54af6239764cb1ac
Christian Broms <cb@christianbroms.com>
2017-02-18 14:32:43 -0800

    Update linkedLists.cpp

--- pointers/linkedLists.cpp
+++ pointers/linkedLists.cpp
@@ -1,107 +1,111 @@
+// Filename: node.cpp
+// Last edit by Christian Broms on 2/18/17
+// Compiler: Xcode 8.2.1
+
 #include <iostream>
 
-using namespace std;
-
 struct Node {
-
-	int data;
-	Node* link;
+    
+    int data;
+    Node* link;
 };
 
-void insertNode(Node* data, Node* hPtr);
+void makeNodeList(Node*& hPtr, Node*& tPtr);	// constructs a new node list
+void makeNode(int p_data, Node* hPtr);		// constructs a new node
+void insertNode(Node* data, Node* hPtr);	// adds a node to the list
+void printListData(Node* hPtr);			// prints the data from the list
 
 int main() {
+    
+    Node* hPtr = NULL;
+    Node* tPtr = NULL;
+	
+	makeNodeList(hPtr, tPtr);
+	
+	makeNode(32, hPtr);
+	makeNode(12, hPtr);
+	makeNode(56, hPtr);
+	makeNode(1, hPtr);
+	makeNode(99, hPtr);
+	makeNode(25, hPtr);
 
-	Node* hPtr = NULL;
-	Node* tPtr = NULL;
-	Node* temp = NULL;
+	printListData(hPtr);
+	
+    return 0;
+}
 
+void makeNodeList(Node*& hPtr, Node*& tPtr) {
+	
 	// make and populate first node (dummy)
-
+	
 	hPtr = new Node;
 	hPtr->data = INT_MIN;
 	hPtr->link = NULL;
 	tPtr = hPtr;
-
-	// make and populate the center nodes
-
-	for (int i = 10; i <= 40; i += 10) {
-
-		temp = new Node;
-		tPtr->link = temp;
-		temp->data = i;
-		temp->link = NULL;
-		tPtr = temp;
-	}
-
+	
 	// make and populate last node (dummy)
-
+	
+	Node* temp = NULL;
 	temp = new Node;
 	tPtr->link = temp;
 	temp->data = INT_MAX;
 	temp->link = NULL;
 	tPtr = temp;
-
-	// add new nodes 
-
-	temp = new Node;
-	temp->data = 25;
-	temp->link = NULL;
-
-	insertNode(temp, hPtr);
-
-	temp = new Node;
-	temp->data = 1;
-	temp->link = NULL;
-
-	insertNode(temp, hPtr);
-
-	temp = new Node;
-	temp->data = 99;
-	temp->link = NULL;
-
-	insertNode(temp, hPtr);
-
-	// display everything that is not INT_MAX or INT_MIN -
-	// these nodes are still part of the list, just not displayed
-
-	temp = hPtr;
-
-	while (temp != NULL) {
-
-		if (temp->data != INT_MAX && temp->data != INT_MIN) {
-
-			cout << temp->data << endl;
-		}
-
-		temp = temp->link;
-	}
-
-	return 0;
 }
 
-// insert passed node into linked list
+void makeNode(int p_data, Node* hPtr) {
+	
+	Node* temp;
+	temp = new Node;
+	temp->data = p_data;
+	temp->link = NULL;
+	insertNode(temp, hPtr);
+	temp = NULL;
+}
+
 
 void insertNode(Node* insert, Node* head) {
+    
+    Node* mPtr = NULL;
+    Node* prior = NULL;
+    bool cont = true;
+    
+    mPtr = head;
+	
+	// insterted according to node's int member
+	
+    while (cont) {
 
-	Node* mPtr = NULL;
-	Node* prior = NULL;
-	bool cont = true;
+        if (mPtr->data < insert->data) {
+            
+            prior = mPtr;
+            mPtr = mPtr->link;
+        }
+        else {
+            cont = false;
+        }
+    }
+    
+    insert->link = prior->link;
+    prior->link = insert;
+}
 
-	mPtr = head;
-
-	while (cont) {
-
-		if (mPtr->data < insert->data) {
-
-			prior = mPtr;
-			mPtr = mPtr->link;
+void printListData(Node* hPtr) {
+	
+	Node* temp = NULL;
+	temp = hPtr;
+	
+	// display everything that is not INT_MAX or INT_MIN
+	
+	while (temp != NULL) {
+	
+		if (temp->data != INT_MAX && temp->data != INT_MIN) {
+		
+			std::cout << temp->data << std::endl;
+			}
+		
+		temp = temp->link;
 		}
-		else {
-			cont = false;
-		}
-	}
-
-	insert->link = prior->link;
-	prior->link = insert;
+	
+	temp = NULL;
 }
