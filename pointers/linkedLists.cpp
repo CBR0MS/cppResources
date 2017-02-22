2d63fa12e302e1f55c1330b8ce464611a82b068c
Christian Broms <cb@christianbroms.com>
2017-02-22 14:14:23 -0800

    integrated function addNode into makeNode

--- pointers/linkedLists.cpp
+++ pointers/linkedLists.cpp
@@ -1,5 +1,5 @@
 // Filename: node.cpp
-// Last edit by Christian Broms on 2/18/17
+// Last edit by Christian Broms on 2/22/17
 // Compiler: Xcode 8.2.1
 
 #include <iostream>
@@ -10,73 +10,68 @@ struct Node {
 	Node* link;
 };
 
-void makeNodeList(Node*& hPtr, Node*& tPtr);	// constructs a new node list
-void makeNode(int p_data, Node* hPtr);		// constructs a new node
-void insertNode(Node* data, Node* hPtr);	// adds a node to the list
-void printListData(Node* hPtr);			// prints the data from the list
+void makeNodeList (Node*& hPtr, Node*& tPtr, Node*& temp);	// constructs a new node list
+void makeNode (int p_data, Node*& hPtr, Node*& temp);		// constructs a new node and adds to the list
+void printListData (Node*& hPtr, Node*& temp);			// prints the data from the list
 
 int main() {
 
 	Node* hPtr = nullptr;
 	Node* tPtr = nullptr;
+	Node* temp = nullptr;
+	hPtr = new Node;
+	tPtr = new Node;
+	temp = new Node;
 
-	makeNodeList(hPtr, tPtr);
+	makeNodeList(hPtr, tPtr, temp);
 
-	makeNode(32, hPtr);
-	makeNode(12, hPtr);
-	makeNode(56, hPtr);
-	makeNode(1, hPtr);
-	makeNode(99, hPtr);
-	makeNode(25, hPtr);
+	makeNode(32, hPtr, temp);
+	makeNode(12, hPtr, temp);
+	makeNode(56, hPtr, temp);
+	makeNode(1, hPtr, temp);
+	makeNode(99, hPtr, temp);
+	makeNode(25, hPtr, temp);
 
-	printListData(hPtr);
+	printListData(hPtr, temp);
 
 	return 0;
 }
 
-void makeNodeList(Node*& hPtr, Node*& tPtr) {
+// constructs a node list with a head and tail pointer
+// data members are INT_MIN and INT_MAX, respectively
 
-	// make and populate first node (dummy)
+void makeNodeList(Node*& hPtr, Node*& tPtr, Node*& temp) {
 
-	hPtr = new Node;
+	// populate first node (dummy)
 	hPtr->data = INT_MIN;
 	hPtr->link = nullptr;
 	tPtr = hPtr;
-
-	// make and populate last node (dummy)
-
-	Node* temp = nullptr;
-	temp = new Node;
+	
+	// populate last node (dummy)
 	tPtr->link = temp;
 	temp->data = INT_MAX;
 	temp->link = nullptr;
 	tPtr = temp;
 }
 
-void makeNode(int p_data, Node* hPtr) {
+// adds a new node to the list, placed based on data 
+// member, in ascending order
 
-	Node* temp;
+void makeNode(int p_data, Node*& hPtr, Node*& temp) {
+
 	temp = new Node;
 	temp->data = p_data;
 	temp->link = nullptr;
-	insertNode(temp, hPtr);
-	temp = nullptr;
-}
-
-
-void insertNode(Node* insert, Node* head) {
 
 	Node* mPtr = nullptr;
 	Node* prior = nullptr;
 	bool cont = true;
 
-	mPtr = head;
-
-	// insterted according to node's int member
+	mPtr = hPtr;
 
 	while (cont) {
 
-		if (mPtr->data < insert->data) {
+		if (mPtr->data < temp->data) {
 
 			prior = mPtr;
 			mPtr = mPtr->link;
@@ -85,27 +80,22 @@ void insertNode(Node* insert, Node* head) {
 			cont = false;
 		}
 	}
-
-	insert->link = prior->link;
-	prior->link = insert;
+	temp->link = prior->link;
+	prior->link = temp;
 }
 
-void printListData(Node* hPtr) {
+// display everything that is not INT_MAX or INT_MIN
 
-	Node* temp = nullptr;
+void printListData (Node*& hPtr, Node*& temp) {
+
 	temp = hPtr;
-
-	// display everything that is not INT_MAX or INT_MIN
 
 	while (temp != nullptr) {
 
 		if (temp->data != INT_MAX && temp->data != INT_MIN) {
 
-			std::cout << temp->data << std::endl;
+			std::cout << temp->data << ' ';
 		}
-
 		temp = temp->link;
 	}
-
-	temp = nullptr;
 }
