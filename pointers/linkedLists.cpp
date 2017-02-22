20f892e0a44dfd0220e6a7a1d6d410b19eecf3f1
Christian Broms <cb@christianbroms.com>
2017-02-21 19:52:11 -0800

    replaced NULL with nullptr

--- pointers/linkedLists.cpp
+++ pointers/linkedLists.cpp
@@ -5,9 +5,9 @@
 #include <iostream>
 
 struct Node {
-    
-    int data;
-    Node* link;
+
+	int data;
+	Node* link;
 };
 
 void makeNodeList(Node*& hPtr, Node*& tPtr);	// constructs a new node list
@@ -16,12 +16,12 @@ void insertNode(Node* data, Node* hPtr);	// adds a node to the list
 void printListData(Node* hPtr);			// prints the data from the list
 
 int main() {
-    
-    Node* hPtr = NULL;
-    Node* tPtr = NULL;
-	
+
+	Node* hPtr = nullptr;
+	Node* tPtr = nullptr;
+
 	makeNodeList(hPtr, tPtr);
-	
+
 	makeNode(32, hPtr);
 	makeNode(12, hPtr);
 	makeNode(56, hPtr);
@@ -30,82 +30,82 @@ int main() {
 	makeNode(25, hPtr);
 
 	printListData(hPtr);
-	
-    return 0;
+
+	return 0;
 }
 
 void makeNodeList(Node*& hPtr, Node*& tPtr) {
-	
+
 	// make and populate first node (dummy)
-	
+
 	hPtr = new Node;
 	hPtr->data = INT_MIN;
-	hPtr->link = NULL;
+	hPtr->link = nullptr;
 	tPtr = hPtr;
-	
+
 	// make and populate last node (dummy)
-	
-	Node* temp = NULL;
+
+	Node* temp = nullptr;
 	temp = new Node;
 	tPtr->link = temp;
 	temp->data = INT_MAX;
-	temp->link = NULL;
+	temp->link = nullptr;
 	tPtr = temp;
 }
 
 void makeNode(int p_data, Node* hPtr) {
-	
+
 	Node* temp;
 	temp = new Node;
 	temp->data = p_data;
-	temp->link = NULL;
+	temp->link = nullptr;
 	insertNode(temp, hPtr);
-	temp = NULL;
+	temp = nullptr;
 }
 
 
 void insertNode(Node* insert, Node* head) {
-    
-    Node* mPtr = NULL;
-    Node* prior = NULL;
-    bool cont = true;
-    
-    mPtr = head;
-	
-	// insterted according to node's int member
-	
-    while (cont) {
 
-        if (mPtr->data < insert->data) {
-            
-            prior = mPtr;
-            mPtr = mPtr->link;
-        }
-        else {
-            cont = false;
-        }
-    }
-    
-    insert->link = prior->link;
-    prior->link = insert;
+	Node* mPtr = nullptr;
+	Node* prior = nullptr;
+	bool cont = true;
+
+	mPtr = head;
+
+	// insterted according to node's int member
+
+	while (cont) {
+
+		if (mPtr->data < insert->data) {
+
+			prior = mPtr;
+			mPtr = mPtr->link;
+		}
+		else {
+			cont = false;
+		}
+	}
+
+	insert->link = prior->link;
+	prior->link = insert;
 }
 
 void printListData(Node* hPtr) {
-	
-	Node* temp = NULL;
+
+	Node* temp = nullptr;
 	temp = hPtr;
-	
+
 	// display everything that is not INT_MAX or INT_MIN
-	
-	while (temp != NULL) {
-	
+
+	while (temp != nullptr) {
+
 		if (temp->data != INT_MAX && temp->data != INT_MIN) {
-		
+
 			std::cout << temp->data << std::endl;
-			}
-		
-		temp = temp->link;
 		}
-	
-	temp = NULL;
+
+		temp = temp->link;
+	}
+
+	temp = nullptr;
 }
