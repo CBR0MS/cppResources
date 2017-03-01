43c6cbdc0f6c05cb06d709c3c3f895e7554692c8
Christian Broms <cb@christianbroms.com>
2017-03-01 11:24:09 -0800

    tests all new changes to class

--- BinarySearchTree/source.cpp
+++ BinarySearchTree/source.cpp
@@ -1,53 +1,51 @@
-/*****   HEADER:  driver.cpp   *****/
+// File: BSTmain.cpp
+// Additional Files: BSTh.hpp
+// Created by Christian Broms on 2/27/17
+// Compiler: Xcode 8.2.1
 
-//Miakhau, Chia
-//2/27/2017
-//MS Visual C++ 2015
-//File needed: BST_H.h
-//Testing BST class template BST
-
-#include "BSTh.h"
+#include "BSTh.hpp"
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	BST<int> myTree1;
-	//for (int i = 0; i < 10; i++)
-	//	myTree1.insert(i);
-
-	myTree1.insert(55);
-	myTree1.insert(20);
-	myTree1.insert(90);
-	myTree1.insert(54);
-	myTree1.insert(60);
-	myTree1.insert(1);
-	myTree1.insert(100);
-	myTree1.insert(52);
-	myTree1.insert(50);
-	myTree1.insert(45);
-	myTree1.insert(64);
-	myTree1.insert(68);
-	myTree1.insert(80);
-
-	cout << "TEST 1: Number Tree...\n";
-	myTree1.inOrder();
+    BST<int> myTree1;
+    for (int i = 0; i < 10; i++)
+		myTree1.insert(i);
+    
+    cout << "TEST 1: Number Tree...\n";
+	cout << "In order...\n";
+    myTree1.inOrder();
+    cout << endl;
+	cout << "Pre order...\n";
+    myTree1.preOrder();
+    cout << endl;
+	cout << "Post order...\n";
+    myTree1.postOrder();
+    
+    cout << "\nDelete 5, 8...\n";
+    myTree1.deleteNum(5);
+	 myTree1.deleteNum(8);
+    myTree1.inOrder();
 	cout << endl;
-	myTree1.preOrder();
+    
+    BST<char> myTree2;
+    for (int i = 0; i < 10; i++)
+    	myTree2.insert(static_cast<char> ('A' + i));
+    cout << "\nTEST 2: Character Tree...\n";
+	cout << "In order...\n";
+	myTree2.inOrder();
 	cout << endl;
-	myTree1.postOrder();
-
-	cout << "Delete 3\n";
-	myTree1.deleteNum(20);
-	myTree1.inOrder();
-
-	//BST<char> myTree2;
-	//for (int i = 0; i < 10; i++)
-	//	myTree2.insert(static_cast<char> ('A' + i));
-	//cout << "\nTEST 2: Character Tree...\n";
-	//myTree2.inOrder();
-
+	cout << "Pre order...\n";
+	myTree2.preOrder();
 	cout << endl;
-	return 0;
+	cout << "Post order...\n";
+	myTree2.postOrder();
+	
+	cout << "\nDelete 'C', 'H'...\n";
+	myTree2.deleteNum('C');
+	myTree2.deleteNum('H');
+	myTree2.inOrder();
+    cout << endl;
+    return 0;
 }
-
