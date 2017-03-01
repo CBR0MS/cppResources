d54edf8e055f9391cd8a7c4e384a7c37f7d45334
Christian Broms <cb@christianbroms.com>
2017-03-01 11:22:39 -0800

    finished delete node function

--- BinarySearchTree/BST.hpp
+++ BinarySearchTree/BST.hpp
@@ -1,221 +1,220 @@
-/*****   HEADER:  BST_H.h   *****/
+// File: BSTh.hpp
+// Additional Files: BSTmain.cpp
+// Created by Christian Broms on 2/27/17
+// Compiler: Xcode 8.2.1
 
-//Christian Broms
-//2/27/2017
-//MS Visual C++ 2015
-//Class template implementation: BST
 #include <iostream>
-#ifndef BSTh_h
-#define BSTh_h
 
-template <class T>
-class BST
-{
+#ifndef BSTh_hpp
+#define BSTh_hpp
+
+template<class T>
+class BST {
+    
 public:
-	BST();
-
-	void insert(T new_item);
-	void deleteNum(T p_delete);
-	void inOrder(); 
-	void preOrder();
-	void postOrder();
-
+    
+    /* CONSTRUCTOR */
+    BST();
+    
+    /* ACESSORS & MUTATORS */
+    void insert(T new_item);
+    void deleteNum(T p_delete);
+    void inOrder();
+    void preOrder();
+    void postOrder();
+    
 private:
-	struct Node
-	{
-		T data;
-		Node *llink = nullptr;
-		Node *rlink = nullptr;
-	};
-
-	Node *m_root = nullptr;
-
-	//Helper functions
-	void helperInOrder(Node *ptr);
-	void helperPreOrder(Node* node);
-	void helperPostOrder(Node* node);
-	bool isEnd(Node* node);
-	
+    
+    struct Node {
+        
+        T data;
+        Node *llink = nullptr;
+        Node *rlink = nullptr;
+    };
+    
+    Node *m_root;
+    
+    /* PRIVATE HELPER FUNCTIONS */
+    void helperInOrder(Node *ptr);
+    void helperPreOrder(Node* node);
+    void helperPostOrder(Node* node);
+    Node* deleteNode(Node *node, int p_data);
+    Node* findMin(Node* node);
+    
 };
 
+/* DEFINITIONS */
+
 template <class T>
-BST<T>::BST()
-{
-	m_root = nullptr;
+BST<T>::BST() {
+    
+    m_root = nullptr;
 }
 
 template <class T>
-void BST<T>::insert(T new_item)
-{
-	//Create the new node for insert item
-	Node *temp = new Node;
-	temp->data = new_item;
-	temp->llink = nullptr;
-	temp->rlink = nullptr;
-
-	//Insert into an empty tree
-	if (m_root == nullptr)
-	{
-		m_root = temp;
-		return;
-	}
-
-	//1. 'trailPtr' trails right behind movPtr as it moves
-	//	down to find the spot for insertion
-	//	REASON: movPtr will become null at some point
-	//		but we need the last node it last traversed to,
-	//		in order to link up the new node.
-	//2. 'left' boolean variable allows us to determine
-	//	if we need to link the new node to the left of right 
-	//	link of an existing node.
-
-	Node *movPtr = m_root;
-	Node *trailPtr = movPtr;
-	bool left = true;
-	while (movPtr != nullptr)
-	{
-		if (movPtr->data > new_item)
-		{
-			trailPtr = movPtr;
-			movPtr = movPtr->llink;
-			left = true;
-		}
-		else if (movPtr->data < new_item)
-		{
-			trailPtr = movPtr;
-			movPtr = movPtr->rlink;
-			left = false;
-		}
-		else if (movPtr->data == new_item)
-			return;
-	}
-
-	//Choose left or right link to link up
-	//	the new node pointed to by 'temp'
-	if (left)
-		trailPtr->llink = temp;
-	else
-		trailPtr->rlink = temp;
-
-	temp = nullptr;
+void BST<T>::insert(T new_item) {
+    
+    // create the new node for insert item
+    Node *temp = new Node;
+    temp->data = new_item;
+    temp->llink = nullptr;
+    temp->rlink = nullptr;
+    
+    // insert into an empty tree
+    if (m_root == nullptr) {
+        
+        m_root = temp;
+        return;
+    }
+    
+    Node *movPtr = m_root;
+    Node *trailPtr = movPtr;
+    bool left = true;
+    
+    while (movPtr != nullptr) {
+        
+        if (movPtr->data > new_item) {
+            
+            trailPtr = movPtr;
+            movPtr = movPtr->llink;
+            left = true;
+        }
+        else if (movPtr->data < new_item) {
+            
+            trailPtr = movPtr;
+            movPtr = movPtr->rlink;
+            left = false;
+        }
+        else if (movPtr->data == new_item)
+            return;
+    }
+    
+    // choose left or right link to link up
+    // the new node pointed to by 'temp'
+    if (left)
+        trailPtr->llink = temp;
+    else
+        trailPtr->rlink = temp;
+    
+    temp = nullptr;
 }
 
 template<class T>
 void BST<T>::deleteNum(T p_delete) {
-
-	// find the node in the tree 
-	Node* movPtr = m_root;
-
-	while (movPtr != nullptr) {
-
-		if (movPtr->data > p_delete) {
-
-			movPtr = movPtr->llink;
-		}
-		else if (movPtr->data < p_delete) {
-
-			movPtr = movPtr->rlink;
-		}
-		else if (movPtr->data == p_delete)
-			break;
-	}
-
-	// check for case 1: node is endnode 
-	if (isEnd(movPtr)) {
-
-		// delete node and make parent endnode
-		delete movPtr;
-		movPtr = nullptr;
-		// finished
-	}
-	else {
-
-	// check for case 2: node has children
-
-		while (!isEnd(movPtr)) {
-
-			Node* temp = movPtr;
-			// find next largest in list- once left, all the way to right
-			temp = temp->llink;
-			while (temp->rlink != nullptr) {
-				temp = temp->rlink;
-			}
-			// swap values of largest and node to delete
-			T tempData = movPtr->data;
-			movPtr->data = temp->data;
-			temp->data = tempData;
-
-			// assign node to delete as next node down
-			movPtr = movPtr->rlink;
-
-			// repeat until node is an endnode
-			// case 1
-
-		}
-		delete movPtr;
-		movPtr = nullptr;
-	}
+    
+    deleteNode( m_root, p_delete);
 }
+
 template<class T>
-bool BST<T>::isEnd(Node* node) {
+typename BST<T>::Node*  BST<T>::deleteNode(Node *node, int p_data){
 
-	if (node->rlink == nullptr && node->llink == nullptr || node == nullptr) {
-		return true;
-	}
-	else {
-		return false;
-	}
+    if (node == nullptr) {
+        
+        // task complete
+        return node;
+    }
+    
+    else if (p_data < node->data){
+        
+        // try the left link
+        node->llink = deleteNode(node->llink, p_data);
+    }
+    else if (p_data > node->data){
+        
+        // try the right link
+        node->rlink = deleteNode(node->rlink, p_data);
+    }
+    else {
+        
+        if (node->llink == nullptr && node->rlink == nullptr){
+            
+            // the node is a leaf node
+            delete node;
+            node = NULL;
+        }
+        else if (node->llink == nullptr){
+            
+            // no left node, continue right
+            node = node->rlink;
+        }
+        else if (node->rlink == nullptr){
+            
+            // no right node, continue left
+            node = node->llink;
+        }
+        else {
+            
+            // find the smallest node on the right branch and swap data fields
+            // alternatively, we could find the largest on the left branch
+            Node *temp = findMin(node->rlink);
+            node->data = temp->data;
+            node->rlink = deleteNode(node->rlink, temp->data);
+        }
+    }
+    return node;
 }
-	
-template <class T>
-void BST<T>::inOrder()
-{
-	helperInOrder(m_root);
+
+template<class T>
+typename BST<T>::Node* BST<T>::findMin(Node* node){
+    
+    while (node->llink != nullptr){
+        
+        node = node->llink;
+    }
+    
+    return node;
 }
 
 template <class T>
-void BST<T>::helperInOrder(Node *ptr)
-{
-	if (ptr != nullptr)
-	{
-		helperInOrder(ptr->llink);
-		cout << ptr->data << ' ';
-		helperInOrder(ptr->rlink);
-	}
+void BST<T>::inOrder() {
+    
+    helperInOrder(m_root);
 }
 
 template <class T>
-void BST<T>::preOrder()
-{
-	helperPreOrder(m_root);
+void BST<T>::helperInOrder(Node *ptr) {
+    
+    if (ptr != nullptr) {
+        
+        helperInOrder(ptr->llink);
+        std::cout << ptr->data << ' ';
+        helperInOrder(ptr->rlink);
+    }
+}
+
+template <class T>
+void BST<T>::preOrder() {
+    
+    helperPreOrder(m_root);
 }
 
 template<class T>
 void BST<T>::helperPreOrder(Node *node) {
-
-	if (node != nullptr) {
-
-		cout << node->data << ' ';
-		helperPreOrder(node->llink);
-		helperPreOrder(node->rlink);
-	
-	}
+    
+    if (node != nullptr) {
+        
+        std::cout << node->data << ' ';
+        helperPreOrder(node->llink);
+        helperPreOrder(node->rlink);
+        
+    }
 }
 
 template <class T>
-void BST<T>::postOrder()
-{
-	helperPostOrder(m_root);
+void BST<T>::postOrder() {
+    
+    helperPostOrder(m_root);
 }
+
 template<class T>
 void BST<T>::helperPostOrder(Node* node) {
-
-	if (node != nullptr) {
-
-		helperPreOrder(node->llink);
-		helperPreOrder(node->rlink);
-		cout << node->data << ' ';
-	}
+    
+    if (node != nullptr) {
+        
+        helperPreOrder(node->llink);
+        helperPreOrder(node->rlink);
+        std::cout << node->data << ' ';
+    }
 }
 
-#endif /* BST_h */
+#endif /* BSTh_hpp */
