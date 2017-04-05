b66d2d3afa9323d7df8777a5aa1250141c521c1d
Christian Broms <cb@christianbroms.com>
2017-04-05 10:29:19 -0700

    added net pay by month

--- employeeInheritance/SalariedEmployee.cpp
+++ employeeInheritance/SalariedEmployee.cpp
@@ -1,41 +1,47 @@
-#include <iostream> 
-#include <string> 
-#include "SalariedEmployee.h" 
+#include <iostream>
+#include <string>
+#include "SalariedEmployee.hpp"
 
 using namespace std;
 using namespace semp;
 
 SalariedEmployee::SalariedEmployee()
-	: Employee(), salary(0)
+: Employee(), salary(0)
 {
-	// deliberately empty 
+    // deliberately empty
 }
 SalariedEmployee::SalariedEmployee(const string& theName,
-	const string& theNumber, double theWeeklyPay)
-	: Employee(theName, theNumber), salary(theWeeklyPay)
+                                   const string& theNumber, double theWeeklyPay)
+: Employee(theName, theNumber), salary(theWeeklyPay)
 {
-	// deliberately empty 
+    // deliberately empty
 }
 double SalariedEmployee::getSalary() const
 {
-	return salary;
+    return salary;
 }
 void SalariedEmployee::setSalary(double newSalary)
 {
-	salary = newSalary;
+    salary = newSalary;
 }
 void SalariedEmployee::printCheck()
 {
-	Employee::setNetPay(salary * 52);
-	cout << "\n________________________________________________\n";
-	cout << " Pay to the order of " << Employee::getName() << endl;
-	cout << " The sum of " << Employee::getNetPay() << " Dollars\n";
-	cout << "_________________________________________________\n";
-	cout << " Check Stub NOT NEGOTIABLE \n";
-	cout << " Employee Number: " << Employee::getSsn() << endl;
-
-	cout << " Salaried Employee. Regular Pay: "
-		<< salary << endl;
-	cout << "_________________________________________________\n";
-
+    
+    // if the net pay hasn't been already set, multiply the weekly salary by four to
+    // get the month's net salary
+    if (netPay == 0.0) {
+    Employee::setNetPay(salary * 4);
+    }
+    
+    cout << "\n________________________________________________\n";
+    cout << " Pay to the order of " << Employee::getName() << endl;
+    cout << " The sum of " << Employee::getNetPay() << " Dollars\n";
+    cout << "_________________________________________________\n";
+    cout << " Check Stub NOT NEGOTIABLE \n";
+    cout << " Employee Number: " << Employee::getSsn() << endl;
+    
+    cout << " Salaried Employee. Regular Pay: "
+    << salary << endl;
+    cout << "_________________________________________________\n";
+    
 }
