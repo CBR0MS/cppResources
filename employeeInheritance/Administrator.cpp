b536371e2f45d6d99e2a6e23795546a9c45b4262
Christian Broms <cb@christianbroms.com>
2017-04-05 10:30:31 -0700

    more monthly net pay

--- employeeInheritance/Administrator.cpp
+++ employeeInheritance/Administrator.cpp
@@ -1,54 +1,66 @@
+// File: Administrator.cpp
+
 #include <iostream>
 #include <string>
-#include "Administrator.h"
+#include "Administrator.hpp"
 
 using namespace std;
 using namespace adm;
 
 Administrator::Administrator()
-	: SalariedEmployee()
-{ 
-	// empty
+: SalariedEmployee()
+{
+    // deliberately empty
 }
 Administrator::Administrator(const string& theName, const string& theSsn, double theWeeklySalary,
-	const string& theTitle, const string& theSupervisor, const string& theResponsibility) 
-	: SalariedEmployee(theName, theSsn, theWeeklySalary), supervisor(theSupervisor),
-	title(theTitle), responsiblity(theResponsibility)
+                             const string& theTitle, const string& theSupervisor, const string& theResponsibility)
+: SalariedEmployee(theName, theSsn, theWeeklySalary), supervisor(theSupervisor),
+title(theTitle), responsiblity(theResponsibility)
 {
-	// empty
+    // deliberately empty
 }
 void Administrator::inputData() {
-
-	cout << "Enter Name: ";
-	cin >> name;
-	cout << "Enter SSN: ";
-	cin >> ssn;
-	cout << "Enter Weekly Salary: ";
-	cin >> salary;
-	cout << "Enter Title: ";
-	cin >> title;
-	cout << "Enter Responsiblity: ";
-	cin >> responsiblity;
-	cout << "Enter Supervisor: ";
-	cin >> supervisor;
-	cout << endl;
+    
+    // we want to input all the parameters of the contructor one by one
+    cout << "Enter Name: ";
+    string name;
+    cin >> name;
+    SalariedEmployee::setName(name);
+    cout << "Enter SSN: ";
+    string ssn;
+    cin >> ssn;
+    SalariedEmployee::setSsn(ssn);
+    cout << "Enter Weekly Salary: ";
+    double salary = 0;
+    cin >> salary;
+    SalariedEmployee::setSalary(salary);
+    cout << "Enter Title: ";
+    cin >> title;
+    cout << "Enter Responsiblity: ";
+    cin >> responsiblity;
+    cout << "Enter Supervisor: ";
+    cin >> supervisor;
+    cout << endl;
 }
 void Administrator::setSupervisor(const string& supName) {
-
-	supervisor = supName;
+    
+    supervisor = supName;
 }
 void Administrator::print() {
-
-	Employee::setNetPay(SalariedEmployee::getSalary() * 52);
-	cout << "Name: " << Employee::getName() << endl;
-	cout << "SSN: " << Employee::getSsn() << endl;
-	cout << "Net Pay : " << Employee::getNetPay() << endl;
-	cout << "Weekly Salary: " << SalariedEmployee::getSalary () << endl;
-	cout << "Title: " << title << endl;
-	cout << "Responsibility: " << responsiblity << endl;
-	cout << "Supervisor: " << supervisor << endl;
+    
+    // because the check is printed for a month, we'll multiply the weekly salary by four
+    Employee::setNetPay(SalariedEmployee::getSalary() * 4);
+    
+    // print all the administrator information
+    cout << "Name: " << Employee::getName() << endl;
+    cout << "SSN: " << Employee::getSsn() << endl;
+    cout << "Net Pay : " << Employee::getNetPay() << endl;
+    cout << "Weekly Salary: " << SalariedEmployee::getSalary () << endl;
+    cout << "Title: " << title << endl;
+    cout << "Responsibility: " << responsiblity << endl;
+    cout << "Supervisor: " << supervisor << endl;
 }
 void Administrator::printCheck() {
-	
-	SalariedEmployee::printCheck();
+    
+    SalariedEmployee::printCheck();
 }
