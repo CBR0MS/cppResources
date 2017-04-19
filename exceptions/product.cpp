753ac77d9d026df2ebd32b8777660e3b45f165f0
Christian Broms <cb@christianbroms.com>
2017-04-19 14:32:21 -0400

    functional main

--- exceptions/product.cpp
+++ exceptions/product.cpp
@@ -3,17 +3,18 @@
 #pragma once
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-class ProductNotFoundException
-{
+class ProductNotFoundException {
+    
 public:
-	ProductNotFoundException() { };
-	ProductNotFoundException(string p_message) { message = p_message };
-	string what() { return message; };
-
+    ProductNotFoundException() { };
+    ProductNotFoundException(string p_message) { message = p_message; };
+    string what() { return message; };
+    
 private:
-	string message;
+    string message;
 };
 
 // Driver.cpp
@@ -24,34 +25,49 @@ private:
 
 using namespace std;
 
-// Given function in the question. You need to throw a
-// ProductNotFoundException instead of returning -1
-
-// TODO: figure out how to return exception and int
-int getProductID(int ids[], string names[], int numProducts, string target) throw(ProductNotFoundException)
-{
-	for (int i = 0; i < numProducts; i++)
-	{
-		if (names[i] == target)
-			return ids[i];
-	}
-	return ProductNotFoundException("Exception: The product is not included in the array!"; //Not found
+int getProductID(int ids[], string names[], int numProducts, string target) throw(...) {
+    
+    for (int i = 0; i < numProducts; i++) {
+        if (names[i] == target)
+            return ids[i];
+    }
+    throw ProductNotFoundException("Exception: The product is not included in the array!"); //Not found
 }
-
-// Given test code in the question. You need to provide exception
-// handling code in this main function
-
-int main()
-{
-	int productIds[]{ 4, 5, 8, 10, 13 };
-	string products[]{ "computer", "flash drive",
-		"mouse", "printer", "camera" };
-
-	cout << getProductID(productIds, products, 5, "mouse") << endl;
-	cout << getProductID(productIds, products, 5, "camera") << endl;
-	cout << getProductID(productIds, products, 5, "laptops") << endl;
-
-	cout << endl;
-	return 0;
-
+                                    
+int main() {
+    
+    int productIds[]{ 4, 5, 8, 10, 13 };
+    string products[]{ "computer", "flash drive", "mouse", "printer", "camera" };
+    
+    try {
+        
+        cout << getProductID(productIds, products, 5, "mouse") << endl;
+        cout << getProductID(productIds, products, 5, "camera") << endl;
+        cout << getProductID(productIds, products, 5, "laptops") << endl;
+        
+        cout << endl;
+    }
+    catch (ProductNotFoundException e){
+        
+        cout << e.what() << endl;
+    }
+    catch (overflow_error e) {
+        
+        cout << e.what() << endl;
+    }
+    catch ( underflow_error e) {
+        
+        cout << e.what() << endl;
+    }
+    catch (length_error e) {
+     
+        cout << e.what() << endl;
+    }
+    catch(out_of_range e) {
+        
+        cout << e.what() << endl;
+    }
+    
+    return 0;
+    
 }
